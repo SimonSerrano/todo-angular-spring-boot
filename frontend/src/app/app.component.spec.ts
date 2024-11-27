70423953372db928697d02fee86e43e6fb6eb3c5
Simon Serrano <simon.serrano@busit.com>
2024-11-27 14:59:59 +0100

    test(frontend): fixed app component tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from './model/Task';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
   beforeEach(async () => {
+    httpClientSpy = jasmine.createSpyObj<HttpClient>('HttpClient', ['get', 'post']);
     await TestBed.configureTestingModule({
-      providers: [provideHttpClientTesting()],
-      imports: [AppComponent],
+      providers: [{ provide: HttpClient, useValue: httpClientSpy }],
+      imports: [AppComponent, NoopAnimationsModule],
     }).compileComponents();
+
   });
 
   it('should create the app', () => {
@@ -23,6 +31,17 @@ describe('AppComponent', () => {
   });
 
   it('should render title', () => {
+
+    const expectedTasks: Task[] = [
+      { id: 'toto', title: 'hello' },
+      { id: 'tata', title: 'world' },
+    ];
+
+    httpClientSpy.get.and.returnValue(new Observable((subscriber) => {
+      subscriber.next(expectedTasks);
+      subscriber.complete();
+    }));
+
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
