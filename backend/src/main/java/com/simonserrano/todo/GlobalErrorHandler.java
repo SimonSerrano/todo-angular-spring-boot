d9eb47624eff5687473fae56b60aa4c6f2e745f8
Simon Serrano <simon.serrano@busit.com>
2025-01-24 16:02:41 +0100

    refactor(backend): do not handle exception class yet

--- backend/src/main/java/com/simonserrano/todo/GlobalErrorHandler.java
+++ backend/src/main/java/com/simonserrano/todo/GlobalErrorHandler.java
@@ -3,10 +3,7 @@ package com.simonserrano.todo;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,14 +12,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalErrorHandler {
-
-  private final Log log = LogFactory.getLog(getClass());
-
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<String> handleException(Exception ex) {
-    log.error(ex);
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
-  }
 
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -35,4 +24,5 @@ public class GlobalErrorHandler {
     });
     return errors;
   }
+
 }
